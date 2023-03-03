24b1891b6787dee7aea18af941a5336994262aeb
Mika Miettinen <mika.olavi.miettinen@gmail.com>
2023-03-03 19:28:46 +0200

    1st increment, no command line arguments

---
+++ increment-1.cpp
@@ -0,0 +1,78 @@
+#include<iostream>
+#include<string>
+
+
+
+// PROTOTYPES ***************************************************************************
+void finderFunc();
+
+// END OF PROTOTYPES ---------------------------------------------------------------------------
+
+
+
+
+
+
+
+
+
+int main() {
+
+	finderFunc();
+
+
+
+	return 0;
+}
+
+
+
+
+
+
+
+
+// FUNCTIONS ***************************************************************************
+
+
+
+// FINDER ***************************************************************************
+
+void finderFunc() {
+	std::string dataset = "", input = "";
+
+	while (dataset.length() == 0)
+	{
+		std::cout << "Give a string from which to search for: ";
+		getline(std::cin, dataset);
+	}
+
+	while (input.length() == 0 || input.length() > dataset.length())
+	{
+		std::cout << "Give search string: ";
+		getline(std::cin, input);
+	}
+
+	size_t found = dataset.find(input);
+
+
+
+	if (found != std::string::npos)
+	{
+		std::cout << "'" << input << "' found in '" << dataset << "' in position " << found << std::endl;
+	}
+
+	else
+	{
+		std::cout << "'" << input << "' NOT found in '" << dataset << "'" << std::endl;
+	}
+
+}
+
+
+// END OF FINDER ---------------------------------------------------------------------------
+
+
+// END OF FUNCTIONS ---------------------------------------------------------------------------
+
+
